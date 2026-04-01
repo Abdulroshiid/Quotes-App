e0661b18fdd3bde3b5263a8240deabfc5c09b106
Isyak Abdulrasheed <abdulrasheedisyak44@gmail.com>
2026-04-02 00:10:11 +0100

    Created and wanting to add the routes on a controller

---
+++ controller/data.js
@@ -0,0 +1,19 @@
+const getAllQuotes = (req, res) => {
+  db.all("SELECT * FROM quotes ORDER BY created_at DESC", [], (err, rows) => {
+    if (err) return res.status(500).json({ error: err.message });
+    res.json(rows);
+  });
+};
+
+const addQuotes = (req, res) => {
+  const { text, author } = req.body;
+  if (!text) return res.status(400).json({ error: "Quote text is required" });
+
+  const query = `INSERT INTO quotes (text, author) VALUES (?, ?)`;
+  db.run(query, [text, author || "Anonymous"], function (err) {
+    if (err) return res.status(500).json({ error: err.message });
+    res.json({ id: this.lastID, text, author });
+  });
+};
+
+module.exports = { getAllQuotes, addQuotes };
