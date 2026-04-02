b86360f130de57901233cba3bdf8a1082e922121
Isyak Abdulrasheed <abdulrasheedisyak44@gmail.com>
2026-04-03 00:59:29 +0100

    Made some changes to server.js and router.js

--- router/router.js
+++ router/router.js
@@ -4,7 +4,7 @@ const router = express.Router();
 // Fetching the components from the controller
 const { getAllQuotes, AddQuotes } = require("../controllers/controller.js");
 
-router.get("/api/quotes", getAllQuotes);
-router.post("/api/quotes", AddQuotes);
+router.get("/quotes", getAllQuotes);
+router.post("/quotes", AddQuotes);
 
 module.exports = router;
